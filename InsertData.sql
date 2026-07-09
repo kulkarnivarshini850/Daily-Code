2fa9f67f6017085d8025367957d546a2acc85868
kulkarnivarshini850 <kulkarnivarshini850@gmail.com>
2026-07-09 19:27:40 +0530

    Add files via upload

---
+++ InsertData.sql
@@ -0,0 +1,7 @@
+INSERT INTO Student (StudentID, Name, Age, Department)
+VALUES
+(101, 'Varshini', 20, 'CSE'),
+(102, 'Rahul', 21, 'ECE'),
+(103, 'Anjali', 19, 'IT'),
+(104, 'Kiran', 22, 'CSE'),
+(105, 'Sneha', 20, 'EEE');
